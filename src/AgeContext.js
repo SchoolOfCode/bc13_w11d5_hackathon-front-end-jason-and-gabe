06fd8d41012d75c2adb81a8f1c7a1abafd15c8ba
jasonchalangary <jason42@tuta.io>
2022-12-09 16:03:36 +0000

    copied functions from workshop and adapted to toys

---
+++ src/AgeContext.js
@@ -0,0 +1,3 @@
+import { createContext } from 'react';
+
+export const AgeContext = createContext('0 to 100');
--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useContext, useEffect } from 'react';
+import { AgeContext } from './AgeContext.js';
 
-function App() {
+
+const url = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3000";
+
+
+export default function App() {
+  const [toysList, setToysList] = useState([]);
+  
+
+  useEffect(() => {
+    async function getToysList() {
+      const response = await fetch(`${url}/items`);
+      const data = await response.json(response);
+      console.log(data);
+      setToysList(data.payload);
+    }
+    getToysList();
+  }, []);
+  
+  function List() {
+    const toyItems = toysList.map(item =>
+      <li key={item.id}>
+        <ToyItem props={item} />
+      </li>
+    );
+    return <ul>{toyItems}</ul>;
+  }
+  
+
+  function ToyItem({ props }) {
+    return (
+      <>
+      <h3>{props.title}</h3>
+      <h4>{props.age}</h4>
+      <ToyImage props={props} />
+      </>
+    );
+  }
+
+
+  function ToyImage({ props }) {
+    const imageSize = useContext(AgeContext);
+    console.log(props)
+    return (
+      <img
+        src={props.imageurl}
+        alt={props.title}
+        width={imageSize}
+        height={imageSize}
+      />
+    );
+  }
+
+
+
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <AgeContext.Provider
+    //   value={imageSize}
+    // >
+      <>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={e => {
+              setIsLarge(e.target.checked);
+            }}
+          />
+          Use large images
+        </label>
+        <hr />
+        <List />
+      </>
+    // </AgeContext.Provider>
+  )
 }
 
-export default App;
+
+
+
